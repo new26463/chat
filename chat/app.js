bb00f726ff015da4a6f705d2075d61a01e186b86
new26463 <new26463@gmail.com>
2017-06-16 13:55:51 +0700

    160660

--- chat/app.js
+++ chat/app.js
@@ -1,25 +1,40 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const sever = app.listen(3000)
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const MongoStore = require('connect-mongodb-session')(session)
+// var store = new express.session.MemoryStore()
+const store = new MongoStore({
+  uri : 'mongodb://tinynew:mC3bTzkqVFFcSS4O@chatdb-shard-00-00-azz0h.mongodb.net:27017,chatdb-shard-00-01-azz0h.mongodb.net:27017,chatdb-shard-00-02-azz0h.mongodb.net:27017/tinynewchat?ssl=true&replicaSet=ChatDB-shard-0&authSource=admin',
+  collection : 'session'
+})
 
 app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'views'))
-  console.log('user eiei')
   app.set('view engine', 'jade')
 
 app.get('/', (req, res)=> {
-  console.log('user eiei')
   res.render('index')
 })
 
 app.get('/chat', (req, res)=>{
-  console.log('chat eiei');
   res.render('chatRoom')
 })
 
 app.get('/admin', (req, res)=>{
-  console.log('admin eiei')
   res.render('listRoom')
 })
+
+app.use(session({
+  store:  store,
+  secret: 'chatnajaeieiza',
+  cookie: {
+    magAge:1000*60*60
+  },
+  resave: true,
+  saveUninitialized: false
+}))
+
+module.exports = app
